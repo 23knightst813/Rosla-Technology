00f982cacbbd62fc20ad20e72318127ee0dfa021
23knightst813 <23knightst813@collyers.ac.uk>
2025-03-25 10:36:10 +0000

    add header and styling to base template, update index page, and create script file

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,8 @@
 import logging
 from flask import Flask, render_template, request, flash, redirect, session, url_for, jsonify
 
-app = Flask(__name__, static_folder='../static')
+app = Flask(__name__)
+app.secret_key = 'dev' 
 
 
 @app.route('/')
@@ -12,6 +13,6 @@ if __name__ == "__main__":
     # set_up_db()
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
     # Production server
-    app.run(host="0.0.0.0", port=5000)
+    # app.run(host="0.0.0.0", port=5000)
     # Devlopment server
-    # app.run(debug=True)
+    app.run(debug=True)
